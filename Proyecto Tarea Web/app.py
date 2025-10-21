d3dbbabc8f398278000752dc9556a04db92e0739
paipaux <felipe.barrientos.diaz@gmail.com>
2025-10-21 20:04:51 -0300

    Reworked Tarea 2, now works for tarea 3

--- Proyecto Tarea Web/app.py
+++ Proyecto Tarea Web/app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify, flash
-# Importamos la nueva función
 from database.db import get_ultimos_avisos, get_all_regiones, get_comunas_by_region, create_aviso_sqlalchemy, create_foto_sqlalchemy, allowed_file, get_avisos_paginados, get_total_avisos, get_aviso_by_id, get_fotos_by_aviso_id, UPLOAD_FOLDER, create_contacto_sqlalchemy
 from validadores.validator import validar_aviso
 from datetime import datetime
@@ -52,8 +51,7 @@ def crear_aviso():
         if errores:
             flash("Los siguientes campos son inválidos: " + ", ".join(errores), 'error')
             return redirect(url_for('formulario'))
-
-        # --- LÓGICA REVERTIDA A TU ORIGINAL (LA CORRECTA) ---
+        
         tipo_corregido = request.form.get('select-tipo').lower()
         
         unidad_medida_form = request.form.get('select-medida')
@@ -62,9 +60,7 @@ def crear_aviso():
         elif unidad_medida_form == 'Meses':
             unidad_medida_corregida = 'm'
         else:
-            # Esto maneja el caso si el JS falla y envía 'a' o 'm'
             unidad_medida_corregida = unidad_medida_form
-        # --- FIN DE LA CORRECCIÓN ---
 
         aviso_data = {
             'comuna_id': request.form.get('select-comuna'),
@@ -115,7 +111,7 @@ def crear_aviso():
         contactos_guardados = 0
         for red, ident in zip(redes, identificadores):
             if red and ident:
-                if red == 'x': # Corregir 'x' minúscula a 'X' mayúscula para el ENUM
+                if red == 'x': 
                     red = 'X'
                 print(f"Guardando contacto: {red} - {ident}")
                 create_contacto_sqlalchemy(aviso_id, red, ident)
